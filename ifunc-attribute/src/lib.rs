eb197047d37ba8b331029ac710d2faabdd305b71
Connor Horman <chorman64@gmail.com>
2025-03-21 12:40:58 -0400

    Initial Commit

---
+++ ifunc-attribute/src/lib.rs
@@ -0,0 +1,308 @@
+#![feature(try_find, proc_macro_expand)]
+
+use proc_macro::TokenStream;
+use proc_macro_deterministic_rand::RandomSource;
+use proc_macro2::{Span, TokenStream as TokenStream2};
+use quote::ToTokens;
+use syn::{
+    Abi, BareFnArg, BareVariadic, Error, Expr, FnArg, Ident, ItemFn, LitStr, Meta, MetaList, Token,
+    TypeBareFn,
+    punctuated::{Pair, Punctuated},
+    spanned::Spanned,
+};
+
+enum ExportName {
+    Implicit(Span),
+    Specified(Span, Expr),
+}
+
+impl ExportName {
+    fn span(&self) -> Span {
+        match self {
+            Self::Implicit(span) => *span,
+            Self::Specified(span, _) => *span,
+        }
+    }
+}
+
+fn as_export_name(attr: &Meta) -> Option<(ExportName, bool)> {
+    match attr {
+        Meta::Path(p) if p.is_ident("no_mangle") => {
+            Some((ExportName::Implicit(attr.span()), false))
+        }
+        Meta::NameValue(p) if p.path.is_ident("export_name") => {
+            Some((ExportName::Specified(attr.span(), p.value.clone()), false))
+        }
+        Meta::List(p) if p.path.is_ident("unsafe") => {
+            let meta = p.parse_args::<Meta>().ok()?;
+            as_export_name(&meta).map(|(v, _)| (v, true))
+        }
+        _ => None,
+    }
+}
+
+fn is_cfg(attr: &Meta) -> bool {
+    match attr {
+        Meta::List(l) if l.path.is_ident("cfg") => true,
+        _ => false,
+    }
+}
+
+fn is_doc(attr: &Meta) -> bool {
+    match attr {
+        Meta::List(l) if l.path.is_ident("doc") => true,
+        Meta::NameValue(l)
+            if l.path.is_ident("doc")
+                || l.path.is_ident("deprecated")
+                || l.path.is_ident("must_use") =>
+        {
+            true
+        }
+        Meta::Path(p)
+            if p.is_ident("deprecated") || p.is_ident("must_use") || p.is_ident("cold") =>
+        {
+            true
+        }
+        _ => false,
+    }
+}
+
+fn is_lint(attr: &Meta) -> bool {
+    match attr {
+        Meta::List(l)
+            if l.path.is_ident("allow")
+                || l.path.is_ident("warn")
+                || l.path.is_ident("deny")
+                || l.path.is_ident("forbid")
+                || l.path.is_ident("expect") =>
+        {
+            true
+        }
+        _ => false,
+    }
+}
+
+fn expand_string_at(expansion: TokenStream2) -> syn::Result<String> {
+    let span = expansion.span();
+    let ts: TokenStream = expansion.into();
+
+    let expanded = ts.expand_expr().map_err(|e| syn::Error::new(span, e))?;
+
+    let lit: LitStr = syn::parse(expanded.into())?;
+
+    Ok(lit.value())
+}
+
+#[derive(Default)]
+struct IfuncAttributes {
+    export_name: Option<ExportName>,
+    export_name_has_unsafe: bool,
+    cfg: TokenStream2,
+    doc: TokenStream2,
+    other_attrs: TokenStream2,
+}
+
+fn write_attr(attr: &Meta, list: &mut TokenStream2, cfg_predicate: Option<&Meta>) {
+    if let Some(cfg_predicate) = cfg_predicate {
+        list.extend(quote::quote! { #[cfg_attr(#cfg_predicate, #attr)]})
+    } else {
+        attr.to_tokens(list);
+    }
+}
+
+fn visit_attr(
+    attr: &Meta,
+    attrs: &mut IfuncAttributes,
+    cfg_predicate: Option<&Meta>,
+) -> syn::Result<()> {
+    if let Some((export_name, has_unsafe)) = as_export_name(&attr) {
+        if let Some(cfg_predicate) = cfg_predicate {
+            let ident = attr.path().get_ident().unwrap();
+            return Err(syn::Error::new_spanned(
+                attr,
+                format_args!("cfg_attr({}) is not supported", ident),
+            ));
+        }
+        attrs.export_name = Some(export_name);
+        attrs.export_name_has_unsafe = has_unsafe;
+    } else if is_cfg(attr) || is_lint(attr) {
+        write_attr(attr, &mut attrs.cfg, cfg_predicate)
+    } else if is_doc(attr) {
+        write_attr(attr, &mut attrs.doc, cfg_predicate)
+    } else {
+        match attr {
+            Meta::List(l) if l.path.is_ident("cfg_attr") => {
+                let mut list = l
+                    .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)?
+                    .into_iter();
+                let cfg_predicate_n = list
+                    .next()
+                    .ok_or_else(|| syn::Error::new_spanned(l, "cfg_attr must have a predicate"))?;
+
+                let cfg_predicate = if let Some(cfg_predicate) = cfg_predicate {
+                    Meta::List(MetaList {
+                        path: l.path.clone(),
+                        delimiter: l.delimiter.clone(),
+                        tokens: quote::quote!(all(#cfg_predicate, #cfg_predicate_n)),
+                    })
+                } else {
+                    cfg_predicate_n
+                };
+
+                for nested in list {
+                    visit_attr(&nested, attrs, Some(&cfg_predicate))?;
+                }
+            }
+            _ => write_attr(attr, &mut attrs.other_attrs, cfg_predicate),
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_indirect_func(
+    item: ItemFn,
+    mut rand: RandomSource,
+) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let sig = item.sig.clone();
+
+    let vis = item.vis;
+
+    let real_sig = item.sig;
+
+    let mut attrs = IfuncAttributes::default();
+    let name = sig.ident;
+
+    let body = item.block;
+
+    for attr in item.attrs {
+        visit_attr(&attr.meta, &mut attrs, None)?;
+    }
+
+    let IfuncAttributes {
+        export_name,
+        export_name_has_unsafe,
+        cfg,
+        doc,
+        other_attrs,
+    } = attrs;
+
+    let block_abi = sig.abi.clone().unwrap_or(Abi {
+        extern_token: Default::default(),
+        name: Some(LitStr::new("Rust", Span::mixed_site())),
+    });
+
+    let fn_ty: TypeBareFn = TypeBareFn {
+        lifetimes: None,
+        unsafety: sig.unsafety,
+        abi: sig.abi.clone(),
+        fn_token: sig.fn_token,
+        paren_token: sig.paren_token,
+        inputs: sig
+            .inputs
+            .into_pairs()
+            .map(|p| {
+                let (a, punct) = p.into_tuple();
+
+                let b = match a {
+                    FnArg::Receiver(s) => {
+                        return Err(Error::new(
+                            s.span(),
+                            "Cannot use `#[indirect_func]` on a method with a reciever",
+                        ));
+                    }
+                    FnArg::Typed(ty) => BareFnArg {
+                        attrs: ty.attrs,
+                        name: None,
+                        ty: *ty.ty,
+                    },
+                };
+
+                Ok(Pair::new(b, punct))
+            })
+            .collect::<syn::Result<_>>()?,
+        variadic: sig.variadic.map(|v| BareVariadic {
+            attrs: v.attrs,
+            name: None,
+            dots: v.dots,
+            comma: None,
+        }),
+        output: sig.output,
+    };
+
+    let safe = match sig.unsafety {
+        Some(un) => quote::quote_spanned! {un.span => #un},
+        None => quote::quote! { safe },
+    };
+
+    let resolver_name = Ident::new("__ifunc_resolver", name.span());
+
+    let sym_name = if let Some(export_name) = export_name {
+        if !export_name_has_unsafe {
+            return Err(syn::Error::new(
+                export_name.span(),
+                "Must use `unsafe` attribute with no_mangle/export_name",
+            ));
+        }
+        match export_name {
+            ExportName::Implicit(span) => {
+                let name = LitStr::new(&format!("{name}"), span);
+                quote::quote! {#name}
+            }
+            ExportName::Specified(_, name) => quote::quote! {#name},
+        }
+    } else {
+        let val = rand.next(name.span());
+
+        let prefix =
+            expand_string_at(quote::quote_spanned! {name.span() => ::core::module_path!()})?;
+
+        let prefix = prefix.replace("::", ".");
+
+        let name_str = format!(".{prefix}.{name}.__IFR__.N{val:016x}");
+        let name = LitStr::new(&name_str, name.span());
+        quote::quote! {#name}
+    };
+
+    Ok(quote::quote! {
+
+        unsafe #block_abi {
+            #cfg
+            #doc
+            #[link_name = #sym_name]
+            #vis #safe #real_sig;
+        }
+
+        const _: () = {
+            #cfg
+            mod __internal {
+
+                use super::*;
+
+                #other_attrs
+                #[allow(improper_ctypes_definitions)]
+                extern "C" fn #resolver_name () -> #fn_ty #body
+                ::ifunc::create_resolver!{
+                    #sym_name, #resolver_name
+                }
+            }
+
+        };
+    })
+}
+
+#[proc_macro_attribute]
+pub fn indirect_func(_input: TokenStream, item: TokenStream) -> TokenStream {
+    let item = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let key_span = item.span();
+
+    let rand = RandomSource::with_key_span(key_span);
+
+    let res = match parse_indirect_func(item, rand) {
+        Ok(item) => item,
+        Err(e) => e.into_compile_error(),
+    };
+
+    res.into()
+}
---
+++ src/lib.rs
@@ -0,0 +1,259 @@
+//! Indirect Function (IFUNC/Symbol Resolver) Support in Rust as a proc-macro
+//!
+//! ## How to Use
+//!
+//! To use [`ifunc`][crate], simply apply the [`indirect_func`] attribute to your function.
+//! Then modify the body of the function such that it returns a captureless closure (or a function item/pointer) to the appropriate body (the resolver),
+//!  rather than just directly including the body of the function.  The signature (parameters and return values) is of the function being called
+//!
+//! ```rust
+//! # use ifunc::indirect_func;
+//! #[indirect_func]
+//! pub fn foo() -> i32 {
+//!   || 0
+//! }
+//! # fn main() {
+//! assert_eq!(foo(), 0)
+//! # }
+//! ```
+//!
+//! You can do control flow in the body (though you can't use the values of the parameters or capture local variables with the closure). For example:
+//!
+//! ```rust
+//! # use ifunc::indirect_func;
+//!
+//! #[indirect_func]
+//! pub fn bar() -> i32 {
+//!    if FOO {
+//!        || 1
+//!    } else {
+//!        || 2
+//!    }
+//! }
+//! static FOO: bool = true;
+//! # fn main() {
+//! assert_eq!(bar(), 1)
+//! # }
+//! ```
+//!
+//! The resolver is run once, before the first time the function is called.
+//!
+//! The body is typechecked so that the returned signatures match the function.
+//!
+//! ```rust,compile_fail
+//! # use ifunc::indirect_func;
+//! #[indirect_func]
+//! pub fn baz() -> i32 {
+//!     || 1.0 // Error: Expected `i32`, got `f32`
+//! }
+//! ```
+//!
+//! It is also typechecked so that the closures do not capture anything:
+//!
+//! ```rust,compile_fail
+//! # use ifunc::indirect_func;
+//! # pub fn get_random_number() -> i32 {
+//! # 4 // https://xkcd.com/221/
+//! # }
+//! #[indirect_func]
+//! pub fn baz() -> i32 {
+//!     let val = get_random_number();
+//!     || val
+//! }
+//! ```
+//!
+//! ## Target Function
+//!
+//! When you apply the [`indirect_func`] attribute to a function, there are now two possible functions that can be referred to.
+//! We call these respectively the "Target Function" and the "Resolver".
+//!
+//! When writing an [`indirect_func`] the *signature* of the function is that of the Target Function, and the *body* is that of the Resolver.
+//!
+//! The resolver is an unnameable function with signature `extern "C" fn () -> F`, where `F` is the fn-pointer type corresponding to the signature of the target function.
+//!
+//! ## Why to use it
+//!
+//! The main reason to use this is if you want to test properties of the system before deciding what function to call.
+//! For example, a test on target features to dispatch based on the runtime CPU support.
+//!
+//! ## Target Support
+//!
+//! This crate presently only supports ELF and Mach-O targets.
+//! For ELF it requires support of `STT_GNU_IFUNC` and `R_*_IRELATIVE`. Currently this is hardcoded to linux (and Lilium) targets.
+//! Mach-O is also specifically supported only for apple (Darwin) targets, such as MacOS or iOS.
+//!
+//! If you're writing for a custom target, or for a baremetal target, but know you have access to the appropriate support, you can set `--cfg unsafe_ifunc_allow_target`.
+//! By setting this variable, you assert to [`ifunc`][crate] that the behaviour is correct. By default, it assumes the target is using ELF.
+//! If you are on a Mach-O target that is not Apple, you also need to set `--cfg unsafe_ifunc_binary_format="mach-o"` (You can also explicitly set this cfg to `elf` or unset it).
+//!
+//! Note that these are bare `cfg` values, not cargo features. You must set these in `RUSTFLAGS` when building, rather than with `Cargo.toml`. Also note that they must be set when building the `ifunc` crate.
+//!
+//! ## Export Names
+//!
+//! One use for the [`indirect_func`] attribute is for system libraries. Therefore, you can apply `#[unsafe(no_mangle)]` and `#[unsafe(export_name)]` to the function.
+//! This has the desired behaviour, that the exported function can be called by the appropriate non-mangled name.
+//!
+//! Otherwise, an internal, mangled name is used. The name will be reasonably readable in debugging, but you cannot rely on the precise form of the name.
+//! (Note that collisions are unlikely, but possible in theory)
+//!
+//! Note that the default abi of the resolved function is the same as any other function, `extern "Rust"`. Therefore, it cannot be called from outside of Rust.
+//! If you need to define a system routine using [`indirect_func`], you still have to make the function `extern "C"` (but see [Limitations](#limitations) below)
+//!
+//! Due to limitations in macros, using an `#[unsafe]` attribute is required for `#[no_mangle]` and `#[export_name]` (even when compiling for editions prior to 2024)
+//!
+//! ## Safety
+//!
+//! If the function marked as [`indirect_func`] is `unsafe`, then the resulting function will be unsafe.
+//! However, the resolver will not be marked as unsafe.
+//! Therefore, to use unsafe operations within the body of the resolver itself, you need to explicitly add an `unsafe` block.
+//! You are responsible for ensure the preconditions of the block is met at any point the resolver may be called (which can include before `main`).
+//! In particular, you cannot solely rely on any precondition of the target function.
+//!
+//! ## Attributes
+//!
+//! Attributes applied to the function are divided into three categories:
+//! * Attributes that apply to the target function,
+//! * Attributes that apply to the resolver, and
+//! * Attributes that apply to both.
+//!
+//! The attributes `#[export_name]` and `#[no_mangle]` are treated specially (they are handled as-if they apply to the target function).
+//!
+//! The following attributes apply to the target:
+//! * `#[doc]` attributes
+//! * `#[deprecated]`
+//! * `#[must_use]`
+//! * `#[cold]`
+//!
+//! The following attributes apply to both the target and the resolver:
+//! * `#[cfg]`
+//! * Lint attributes (allow, warn, expect, etc.). Note that `expect` may not have the desired behaviour when applied to an [`indirect_func`].
+//!
+//!
+//! The following attributes apply to the resolver only:
+//! * `#[target_feature]`. Note that this is equivalent to adding `-C target-feature="+<feature-list>"` to the crate flags.
+//! * `#[unsafe(link_section)]` (See limitations for further details).
+//!
+//! Additionally, any attribute not in any of the above lists will default to being applied to the resolver only. This includes tool attributes, and macro attributes.
+//! Attributes
+//!
+//! ## Limitations
+//!
+//! The following limitations are noted. Some may be fixed in a future release:
+//! * It is possible for `#[indirect_func]` to be run before `main` is entered. Some parts of `libstd` may not function properly from inside the resolver, This makes the `ifunc_attribute` `unsafe` (but there is no way to communicate this).
+//! * `#[inline]` does not have any effect on an #\[[`indirect_func`]\]. The attribute can be applied but is silently ignored.
+//!    * This may be fixed in the future with cooperation from the compiler. Such support will likely require the use of LTO.
+//! * The body of the resolver function is not currently modified in any way. Therefore, return values of the resolver must match the signature of the target function. In particular:
+//!    * Parameters must be declared both by the function and the closure,
+//!    * Closures in particular require the abi of the function to be `"Rust"`, and therefore an explicit function must be used if you want to return another abi such as `extern "C"`.
+//! * You need to wrap `#[target_feature]` functions in a wrapper to use.
+//! * Due to the way the resolver is implemented, if the ABI of the function is not `extern "Rust"`, then non-FFI safe types will trigger an `improper_ctypes` lint *not* `improper_ctypes_definitions`
+//!   This may be important if the type is erroneously considered non-FFI safe (a thin raw pointer that is only used as a pointer by FFI).
+//! * `#[track_caller]` is presently a compile-error when applied to an [`indirect_func`].
+//! * `#[link_section]` only affects the actualy resolver function. Some code is generated in addition to the resolver, which will presently be placed in the default section for program code (typically `.text`).
+//! * `#[cfg_attr]` cannot be used with `#[no_mangle]` or `#[export_name]`. Otherwise, it is supported as normal (and propagates to the internal attributes).
+
+#![cfg_attr(not(test), no_std)]
+
+/// Marks a function as being "indirect" and requires the body to resolve the function to be called.
+///
+/// See the [crate-level][`crate`] documentation for details
+///
+/// # Safety
+/// Because of how indirect function resolvers work, you must guarantee two properties (these apply to the resolver itself, not to the target function):
+/// 1. The resolver body must not be reentrant. IE. it must not use the value of the symbol it is trying to resolve (to call it or to convert it to a function pointer),
+/// 2. You must not rely on the timing of the resolver being called (except that it will be called at the latest immediately before calling the function itself).
+///  In particular, you must not rely on features of the standard library that may not work before `main`.
+pub use ifunc_attribute::indirect_func;
+
+#[doc(hidden)]
+pub use core as _core;
+
+#[cfg(not(any(
+    doc,
+    target_os = "linux",
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "tvos",
+    target_os = "watchos",
+    target_os = "visionos",
+    target_os = "lilium",
+    unsafe_ifunc_allow_target
+)))]
+core::compile_error!("Requires an ELF (with GNU_IFUNC support) or Mach-O target to compile");
+
+/// A primitive macro that allows creation of a resolver for `$sym` that calls `$resolver`.
+/// This is typically used via [`indirect_func`], however it can also be called directly.
+///
+/// `$resolver` must have the signature `extern "C" fn() -> F` where `F` is the signature of the to-be-resolved function.
+/// `$sym` is the stringified name of the symbol you want to define.
+///
+/// # Safety
+/// The resolver is run with the same timing as [`indirect_func`] is. Therefore, the constraints on a use if [`indirect_func`] also apply to the specified resolver.
+/// Additionally, you are responsible for ensuring the type signature of `$resolver` matches. No compile-time checks are performed
+#[macro_export]
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "tvos",
+    target_os = "watchos",
+    target_os = "visionos",
+    unsafe_ifunc_binary_format = "mach-o"
+))]
+macro_rules! create_resolver {
+    ($sym:literal, $resolver:ident) => {
+        $crate::_core::arch::global_asm! {
+            $crate::_core::concat!(".symbol_resolver ", $sym),
+            "jmp {resolver}",
+            resolver = sym $resolver
+        }
+    };
+}
+
+/// A primitive macro that allows creation of a resolver for `$sym` that calls `$resolver`.
+/// This is typically used via [`indirect_func`], however it can also be called directly.
+///
+/// `$resolver` must have the signature `extern "C" fn() -> F` where `F` is the signature of the to-be-resolved function.
+/// `$sym` is the stringified name of the symbol you want to define.
+///
+/// # Safety
+/// The resolver is run with the same timing as [`indirect_func`] is. Therefore, the constraints on a use if [`indirect_func`] also apply to the specified resolver.
+/// Additionally, you are responsible for ensuring the type signature of `$resolver` matches. No compile-time checks are performed
+#[macro_export]
+#[cfg(not(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "tvos",
+    target_os = "watchos",
+    target_os = "visionos",
+    unsafe_ifunc_binary_format = "mach-o"
+)))]
+macro_rules! create_resolver {
+    ($sym:literal, $resolver:ident) => {
+        $crate::_core::arch::global_asm! {
+            $crate::_core::concat!(".type ", $sym, ", STT_GNU_IFUNC"),
+            $crate::_core::concat!(".global ", $sym),
+            $crate::_core::concat!($sym,":"),
+            "jmp {resolver}",
+            resolver = sym $resolver
+        }
+    };
+}
+
+#[cfg(test)]
+extern crate self as ifunc;
+
+#[cfg(test)]
+mod test {
+
+    use ifunc_attribute::indirect_func;
+
+    #[indirect_func]
+    fn foo() -> String {
+        || String::from("foo")
+    }
+
+    #[test]
+    fn foo_test() {
+        assert_eq!(foo(), "foo")
+    }
+}
